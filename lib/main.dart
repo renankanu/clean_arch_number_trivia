9bedb32278f050221ab8404b8c8b7dffd101ab26
renankanu <renankanu@gmail.com>
2022-01-05 09:47:21 -0300

    refactor: removing unnecessary files

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
-import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
-    GetMaterialApp(
+    const MaterialApp(
       title: "Application",
-      initialRoute: AppPages.initial,
-      getPages: AppPages.routes,
+      initialRoute: '/',
     ),
   );
 }
