ff77876d2b9809cb9b6f8f53f9c8a2ef0bbe0821
renankanu <renankanu@gmail.com>
2022-01-09 10:56:32 -0300

    feature: create datasources contract in dala layer

---
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -0,0 +1,18 @@
+import 'package:clean_arch/core/errors/failures.dart';
+import 'package:clean_arch/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:clean_arch/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
+  @override
+  Future<Either<Failures, NumberTrivia>> getConcreteNumberTrivia(int number) {
+    // TODO: implement getConcreteNumberTrivia
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failures, NumberTrivia>> getRandomNumberTrivia() {
+    // TODO: implement getRandomNumberTrivia
+    throw UnimplementedError();
+  }
+}
